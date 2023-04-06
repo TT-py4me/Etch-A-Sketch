b26964e44a2b83b883747cfb3b0e607c55d78956
TT-py4me <51959897+TT-py4me@users.noreply.github.com>
2023-04-06 14:36:37 -0400

    Give each grid box a hovering effect

--- etchasketch.js
+++ etchasketch.js
@@ -3,9 +3,14 @@ const container = document.querySelector('.grid_container');
 
 for (let i = 0; i<16; i++) {
     for (let k = 0; k<16; k++) {
+        function changeColor(){
+            content.setAttribute('style', 'background-color:blue')
+            return;
+        }
         const content = document.createElement('div');
         content.classList.add('content');
         content.className = 'box';
+        content.addEventListener('mouseover', changeColor)
         container.appendChild(content); 
     }   
 }
