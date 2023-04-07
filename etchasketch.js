cb063434d818affae2456039f4b252eb81b09108
TT-py4me <51959897+TT-py4me@users.noreply.github.com>
2023-04-07 10:41:23 -0400

    Allow user to change grid size
    Add a button to get the grid size. Replace
    any previous grid with a new grid when
    given a new size. Edit width and height
    to make each box a square, even after
    the hovering effect.

--- etchasketch.js
+++ etchasketch.js
@@ -1,19 +1,50 @@
 const container = document.querySelector('.grid_container');
 
+let size = 16;
 
-for (let i = 0; i<16; i++) {
-    for (let k = 0; k<16; k++) {
+function printGrid(size, width) {
+
+for (let i = 0; i<size; i++) {
+    for (let k = 0; k<size; k++) {
         function changeColor(){
             content.setAttribute('style', 'background-color:blue')
+            content.style.width = width;
+            content.style.height = `${(100/size)*13.5}px`;
             return;
         }
         const content = document.createElement('div');
         content.classList.add('content');
         content.className = 'box';
+        content.style.width = width;
+        content.style.height = `${(100/size)*13.5}px`;
         content.addEventListener('mouseover', changeColor)
         container.appendChild(content); 
     }   
+   
 }
+return;
+}
+
+printGrid(16);
+const button = document.querySelector('.button');
+button.addEventListener('click', changeSize)
+
+function changeSize(e){
+    console.log(e)
+    do {
+    size = window.prompt("Please enter grid size (<100)") 
+    } while(size>99)
+    if(size<100){
+    container.innerHTML='';
+    const width = (100/size) + "%";
+    printGrid(size, width)//
+    }
+}
+
+
+
+
+
 
 
 
